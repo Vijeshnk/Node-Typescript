78ca5fc8e913ff5f495966f4b42e6e6786d3253a
vijeshnk <pace@PW062.local>
2024-01-23 15:22:12 +0530

    added the delete route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUsers } from '../db/users'
+import { deleteUserById, getUsers } from '../db/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
 
@@ -13,4 +13,22 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
         return res.sendStatus(400)
     }
 
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+
+        const { id } = req.params
+
+
+        const deletedUser = await deleteUserById(id)
+
+        return res.json(deletedUser)
+
+
+
+    } catch (error) {
+        console.error(error)
+        return res.sendStatus(400)
+    }
 }
